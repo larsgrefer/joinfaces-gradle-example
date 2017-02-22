82f698c4581a3884d5ba91f00f7cb1807e827104
Lars Grefer <github@larsgrefer.de>
2017-02-23 00:45:44 +0100

    try tomcat deployment on travis

--- war-example/src/main/java/org/joinfaces/example/WarExampleApplication.java
+++ war-example/src/main/java/org/joinfaces/example/WarExampleApplication.java
@@ -2,12 +2,19 @@ package org.joinfaces.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author Lars Grefer
  */
 @SpringBootApplication
-public class WarExampleApplication {
+public class WarExampleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WarExampleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WarExampleApplication.class, args);
